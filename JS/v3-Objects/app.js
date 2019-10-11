5746b528105bd902c64d9029f0261c9fbbeadb12
Sorin Paun <sorin.paun@powercoders.org>
2019-10-11 11:16:59 +0200

    Objects first commit

---
+++ JS/v3-Objects/app.js
@@ -0,0 +1,49 @@
+//MY TODO LIST - GLOBAL SCOPE
+let todoList = [
+  "Learn HTML5", 
+  "Learn CSS", 
+  "Learn JS", 
+  "Learn PHP"
+];
+
+
+//DISPLAY TODOS
+function displayTodos() {
+  console.log(todoList);
+  let bob = 3; // LOCAL SCOPE - not available outside the function
+}
+// displayTodos();
+
+
+//ADD TODO
+function addTodo(todoText) {
+  todoList.push(todoText);
+  displayTodos();
+}
+// addTodo("Do that!");
+// addTodo("Do this!");
+// addTodo("Do NOT do that!");
+
+
+//CHANGE TODO
+function changeTodo(index, text) {
+  todoList[index] = text;
+  displayTodos(); 
+}
+// changeTodo(0, "Some todo");
+// changeTodo(2, "Something like this");
+
+
+//DELETE TODO
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  displayTodos();
+}
+// deleteTodo(1);
+// deleteTodo(3);
+
+
+
+
+
+
---
+++ JS/v3-Objects/objects.js
@@ -0,0 +1,28 @@
+//A SIMPLE OBJECT
+let simpleObject = {
+
+  myNumber    : 35,
+  myString    : "Lorem ipsum ipso facto",
+  myArray     : [2,3,4,"Bla"],
+  myFunction  : function () {
+    return "Something from a function";
+  },
+  myObject    : {
+    name : "Sorin Paun",
+    country: "Romania" 
+  }
+
+};
+
+// console.log( "My Object:", simpleObject );
+// console.log( "My number:", simpleObject.myNumber );
+// console.log( "My Array:", simpleObject.myArray );
+// console.log( "My Array Index:", simpleObject.myArray[1] );
+
+// console.log( "My Function:", simpleObject.myFunction() );
+
+// console.log( "My Inside Object key:", simpleObject.myObject.name );
+// console.log( "My Inside Object key:", simpleObject.myObject.country );
+
+
+//MY PERSONS OBJECT
