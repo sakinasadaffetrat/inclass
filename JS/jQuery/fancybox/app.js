662ab2dacdcb2ad2f8818de14cc4e18a116febf8
Sorin Paun <sorin.paun@powercoders.org>
2019-11-06 14:06:44 +0100

    Latest changes on Week 7 / Day 3

--- JS/jQuery/fancybox/app.js
+++ JS/jQuery/fancybox/app.js
@@ -7,4 +7,24 @@ $(function() {
     $('body').toggleClass('menu-is-open');
   });
 
+  $('[data-fancybox="gallery"]').fancybox({
+    loop: true,
+    arrows: true,
+    infobar: false,
+    toolbar: true,
+    buttons: [
+      //"zoom",
+      //"share",
+      //"slideShow",
+      //"fullScreen",
+      //"download",
+      //"thumbs",
+      "close"
+    ],
+    animationEffect: "zoom-in-out",
+    animationDuration: 600,
+    transitionEffect: "tube",
+    transitionDuration: 500,
+  });
+
 });
