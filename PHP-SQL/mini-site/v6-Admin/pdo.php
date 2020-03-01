be23c89b52e6cd574b4123d865067317f0e82299
PowerCoders Lausanne <47698182+powercoders-lausanne@users.noreply.github.com>
2020-03-01 23:08:48 +0100

    Renamed DB

--- PHP-SQL/mini-site/v6-Admin/pdo.php
+++ PHP-SQL/mini-site/v6-Admin/pdo.php
@@ -9,7 +9,7 @@
 /* DB INFO
 ------------------------------------------*/
 $db_host    = '127.0.0.1';
-$db_name    = 'poco_minisite_2';
+$db_name    = 'poco_minisite';
 $db_user    = 'root';
 $db_pass    = ''; //On XAMPP by default is '' → empty string; on XAMPP & MAMP by default is 'root'
 $db_charset = 'utf8mb4';
